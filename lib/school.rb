d72a93138607f1a79028ad9ac152cadc128ddf83
Sara Walker <sarawall1187@gmail.com>
2019-01-03 19:47:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_accessor :roster
+   
+  def initialize(school)
+    @school = school 
+    @roster = {}
+  end
+  
+   def add_student(name, grade)
+     @add_student = {
+       name: grade
+     }
+     @roster << add_student(name, grade)
+   end
+   
+end
+
